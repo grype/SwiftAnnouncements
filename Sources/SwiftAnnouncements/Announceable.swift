ca2b769ee4d1ff38f833e4b9ae4cd7f5b5b0ae38
Pasha <pasha@2vega.com>
2021-04-11 17:59:34 -0700

    Rename Announcement to Announceable

---
+++ Sources/SwiftAnnouncements/Announceable.swift
@@ -0,0 +1,30 @@
+//
+//  Announceable.swift
+//  
+//
+//  Created by Pavel Skaldin on 4/8/21.
+//
+
+import Foundation
+
+/**
+ I define how to be Announceable.
+ 
+ Announceables can be used for notifying observers of an `Announcer`.
+ */
+public protocol Announceable {
+    /// Optional logic to perform immediately before delivering an announcement
+    func prepareForAnnouncement()
+    /// Answers whether an announcement of this type should be handled
+    static func handles<T:Announceable>(_ anAnnouncement: T) -> Bool
+}
+
+extension Announceable {
+    public func prepareForAnnouncement() {
+        // nothing to do
+    }
+    
+    public static func handles<T:Announceable>(_ anAnnouncement: T) -> Bool {
+        return type(of: anAnnouncement) == self
+    }
+}
